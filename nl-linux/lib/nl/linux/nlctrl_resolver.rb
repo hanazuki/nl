33d3d3e186739ff26ba4835d80df9ddad37f367c
Kasumi Hanazuki <kasumi@rollingapple.net>
2026-09-08 16:08:43 +0000

    doc: NlctrlResolver

--- nl-linux/lib/nl/linux/nlctrl_resolver.rb
+++ nl-linux/lib/nl/linux/nlctrl_resolver.rb
@@ -1,28 +1,28 @@
-# frozen_string_literal: true
-
 require 'nl/linux/nlctrl'
 
 module Nl
   module Linux
     # Resolves Generic Netlink family metadata through nlctrl.
     class NlctrlResolver
-      #--
-      # @rbs client: ::Nl::Genl::Client
-      # @rbs name: String
-      # @rbs return: ::Nl::Genl::FamilyInfo
-      def call(client, name)
-        if name == Nlctrl::NAME
-          return Nl::Genl::FamilyInfo.new(
-            id: Nl::Genl::GENL_ID_CTRL,
-            multicast_groups: {}.freeze,
-          )
-        end
+      FAMILY_INFO_CTRL = Nl::Genl::FamilyInfo.new(
+        id: Genl::GENL_ID_CTRL,
+        multicast_groups: {}.freeze,
+      ).freeze
 
-        reply = client.family(Nlctrl).do_getfamily(family_name: name)
+      # Resolves a Generic Netlink family metadata through nlctrl.
+      #
+      # @param [Genl::Client] client
+      # @param [String] family_name
+      # @return [Genl::FamilyInfo]
+      # @rbs (Genl::Client client, String family_name) -> Genl::FamilyInfo
+      def call(client, family_name)
+        return FAMILY_INFO_CTRL if family_name == Nlctrl::NAME
+
+        reply = client.family(Nlctrl).do_getfamily(family_name:)
         groups = (reply.mcast_groups || []).to_h do |attributes|
           [attributes[:name].value, attributes[:id].value]
         end
-        Nl::Genl::FamilyInfo.new(id: reply.family_id, multicast_groups: groups.freeze)
+        Genl::FamilyInfo.new(id: reply.family_id, multicast_groups: groups.freeze)
       end
     end
 
